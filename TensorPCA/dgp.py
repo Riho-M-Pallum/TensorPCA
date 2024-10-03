f2bf8bc9c800983a17f5d34813d609cba0549c2c
Riho <riho889@gmail.com>
2024-10-03 20:17:43 +0200

    Addd original DGP back and renamed alternative to DGP_alt

--- TensorPCA/dgp.py
+++ TensorPCA/dgp.py
@@ -11,7 +11,60 @@ from numpy import linalg as LA
 from scipy.stats import ortho_group
 from .base import factor2tensor
 
-def DGP(shape, R, ar_coefficients, seed, f_std = 1, error_std = 1):
+def DGP(shape, R):
+    """
+    Generates the tensor data as a strong factor model introduced in the paper.
+    The first dimension is treated as temporal, and generates AR(1) process factors.
+
+    Parameters
+    ----------
+    shape : tupe
+        shape of the tensor.
+    R : int
+        rank or number of factors.
+
+    Returns
+    -------
+    Tensor.
+
+    """
+    d = len(shape)
+    
+    T = shape[0]
+    
+    # generating factor
+    rho = 0.5
+    sig_e = 0.1
+    F = np.empty((T+100,R))
+    e = sig_e * np.random.normal(0,1,(T+100,R))
+    F[0,:] = e[0,:]
+    for t in range(1,T+100):
+        F[t,:] = F[t-1,:] * rho + e[t,:]
+    F = F[100:,:]
+    for r in range(R):
+        F[:,r] = F[:,r] / LA.norm(F[:,r])
+        
+    # generating loadings
+    M = [F]
+    for j in range(1,d):
+        M.append(ortho_group.rvs(shape[j])[:,0:R])
+        
+    # generating scale component, singal strength
+    s = np.sqrt(np.prod(shape)) * np.array(range(R,0,-1))
+    # s = np.sqrt(np.prod(shape)) * np.array([2,0.8])
+    
+    Y = factor2tensor(s,M)
+    
+    # generating idiosyncratic noise
+    sig_u = 1
+    U = sig_u * np.random.normal(0,1,shape)
+    
+    # add noise to tensor Y
+    Y = Y + U
+    
+    return Y, s, M
+
+def DGP_alt(shape, R, ar_coefficients, seed, f_std = 1, error_std = 1):
     """
     Generates the tensor data as a strong factor model introduced in the paper.
     The first dimension is treated as temporal, and generates AR(1) process factors.
@@ -53,7 +106,7 @@ def DGP(shape, R, ar_coefficients, seed, f_std = 1, error_std = 1):
     M = [F]
     for j in range(1,d):
         M.append(ortho_group.rvs(shape[j])[:,0:R])
-      
+    print(shape(M))
     # generating scale component, singal strength
     s = np.sqrt(np.prod(shape)) * np.array(range(R,0,-1))
     # s = np.sqrt(np.prod(shape)) * np.array([2,0.8])
